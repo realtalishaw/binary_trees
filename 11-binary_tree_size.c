b638810bb2aaaf880abbb269e4bee374579afd9d
realtalishaw <misstalishawhite@gmail.com>
2021-03-09 21:36:26 +0000

    stuff

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - This finds the size of a binary tree by counting all it nd
+ * @tree: This is a pointer to the struct
+ * Return: 
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return(binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+}
---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+
+}
