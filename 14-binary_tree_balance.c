4740856d4596a2878fe4374e57d42d63894eac9e
realtalishaw <misstalishawhite@gmail.com>
2021-03-11 15:55:54 +0000

    fixed helper function

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,38 +1,42 @@
 #include "binary_trees.h"
 /**
- * 
- * 
- * 
+ * binary_tree_balance - balance
+ * @tree: fs
+ * Return: return
  */
- int binary_tree_balance(const binary_tree_t *tree)
- {
-     int l_height = 0, r_height = 0;
+size_t height(const binary_tree_t *tree);
 
-     if (tree == NULL)
-     {
-         return (0);
-     }
-    l_height += binary_tree_height(tree->left);
-    r_height += binary_tree_height(tree->right);
-    return (l_height - r_height);
- }
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int l_height = 0, r_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	l_height += height(tree->left);
+	r_height += height(tree->right);
+	return (l_height - r_height);
+}
+
 /**
- * main - Entry point
- *
+ * height - Entry point
+ * @tree: tree
  * Return: Always 0 (Success)
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+
+size_t height(const binary_tree_t *tree)
 {
 	size_t hl, hr;
-    if(tree == NULL)
-		return (-1);
-    hl = binary_tree_height(tree->left);
-    hr = binary_tree_height(tree->right);
-    if (hl > hr)
-    {
-        return (hl + 1);
-    }
-    else
-	    return (hr + 1);
 
+	if (tree == NULL)
+		return (-1);
+	hl = height(tree->left);
+	hr = height(tree->right);
+	if (hl > hr)
+	{
+		return (hl + 1);
+	}
+	else
+		return (hr + 1);
 }
