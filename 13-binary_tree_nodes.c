7de83f4d56378a3e97bde2db32241e7ce220232c
realtalishaw <misstalishawhite@gmail.com>
2021-03-11 15:48:54 +0000

    betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
+ * binary_tree_nodes - Entry point
+ * @tree: tree
  * Return: Always 0 (Success)
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 
-    if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-    	return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
 	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 
