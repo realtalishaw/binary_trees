4f92aab606ca1bf6d740e969b20a4bca4c0436c3
realtalishaw <misstalishawhite@gmail.com>
2021-03-11 19:12:13 +0000

    fix betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,5 +7,23 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	if (!node)
+		return (NULL);
+
+	return(binary_tree_sibling(node->parent));
+}
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+        if (!node || !node->parent)
+                return (NULL);
+
+        if (node->parent->left == NULL || node->parent->right == NULL)
+		return (NULL);
+
+        if (node->parent->right == node)
+                return (node->parent->left);
+        else
+                return (node->parent->right);
 
 }
