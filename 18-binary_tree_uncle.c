9d1b71c456be2511ebd1ccc7fe8b4f06fac2a080
CSant04y <esquivelcarlos27893@gmail.com>
2021-03-11 19:27:35 +0000

    added betty fixes to file

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
+ * binary_tree_uncle - This function check to see if a node is an uncle.
+ * @node: the node that is passed
  * Return: Always 0 (Success)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
@@ -10,20 +10,25 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (!node)
 		return (NULL);
 
-	return(binary_tree_sibling(node->parent));
+	return (binary_tree_sibling(node->parent));
 }
+/**
+ * binary_tree_sibling - This checks to see ifa node is a sibiling
+ * @node: node
+ * Return: Always 0 (Success)
+ */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (!node || !node->parent)
-                return (NULL);
+		if (!node || !node->parent)
+			return (NULL);
 
-        if (node->parent->left == NULL || node->parent->right == NULL)
-		return (NULL);
+		if (node->parent->left == NULL || node->parent->right == NULL)
+			return (NULL);
 
-        if (node->parent->right == node)
-                return (node->parent->left);
-        else
-                return (node->parent->right);
+		if (node->parent->right == node)
+			return (node->parent->left);
+		else
+			return (node->parent->right);
 
 }
