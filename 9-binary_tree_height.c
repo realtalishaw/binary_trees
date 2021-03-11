2fc0e6f6d36aa41b371fd2c2447915e0ae0d7350
realtalishaw <misstalishawhite@gmail.com>
2021-03-11 15:42:00 +0000

    checking

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,22 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * main - Entry point
- *
+ * findMax - Entry point
+ * @a: a
+ * @b: b
  * Return: Always 0 (Success)
  */
+
+int findMax(int a, int b);
+
+int findMax(int a, int b)
+{
+	if(a >= b)
+		return a;
+	return b;
+}
+
+/**
+ * binary_tree_height - some height stuff
+ * @tree: tree
+ * Return: something I'm sure
+ */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t hl, hr;
-    if(tree == NULL)
-		return (-1);
-    hl = binary_tree_height(tree->left);
-    hr = binary_tree_height(tree->right);
-    if (hl > hr)
-    {
-        return (hl + 1);
-    }
-    else
-	    return (hr + 1);
+	if(tree == NULL || (tree->right == NULL && tree->left == NULL))
+		return 0;
 
+	return (findMax(binary_tree_height(tree->left), binary_tree_height(tree->right)) + 1);
 }
